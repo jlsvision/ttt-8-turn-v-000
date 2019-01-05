79e36cc4d35c66932bf6625c3368fea488c46368
Jorge Sanchez <sanchez0594.js@gmail.com>
2019-01-05 00:34:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,41 +8,52 @@ def display_board(board)
 end
 
 #coverts a user's place on the board to the index integer
-def input_to_index(input)
-  input.to_i - 1
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
 end
 
 #checks the number to see if 1) the position is already taken and 2)it is an actual number
 def valid_move?(board, index)
-  def position_taken?(array,ind)
-  if array[ind] == " " || array [ind] == "" || array[ind] == nil
-    return false
-  else return true
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
   end
-end
 
-def on_board(num)
-  if num.between?(0, 8) == true
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
   else
     return false
   end
 end
 
-if (position_taken?)(board, index) == false && (on_board?(index) == true)
-
+#Takes a users num and the board, and places the X or O char in that position
 def move(board, index, character = "X")
   board[index] = character
-  return on_board
+  return board
 end
 
-def turn (board)
+#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
+def turn(board)
   puts "Please enter 1-9:"
-num = gets.strip
-index = input_to_index(num)
-if valid_move?(board, index) == true
-  move(board, index)
-  display_board(board)
-else
-  turn(board)
+  num = gets.strip
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
